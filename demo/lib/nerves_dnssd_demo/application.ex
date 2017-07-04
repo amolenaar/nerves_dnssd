9f70edb7ff7d7925252376aa825753f2d32ed1c1
Arjan Molenaar <gaphor@gmail.com>
2017-07-04 23:57:11 +0200

    Clean up demo app

--- demo/lib/nerves_dnssd_demo/application.ex
+++ demo/lib/nerves_dnssd_demo/application.ex
@@ -3,10 +3,11 @@ defmodule NervesDnssdDemo.Application do
 
   @interface :eth0
 
+  require Logger
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
 
     if Application.get_env(:nerves_dnssd_demo, :networking, true) do
       Nerves.Networking.setup @interface
@@ -14,19 +15,11 @@ defmodule NervesDnssdDemo.Application do
 
     announce_node()
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # worker(NervesMdnsDemo.Worker, [arg1, arg2, arg3]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: NervesDnssdDemo.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, self()}
   end
 
   def announce_node do
-    IO.puts "Registering node as #{:erlang.node}\n"
+    Logger.info "Registering node as #{:erlang.node}"
     {:ok, local_ip} = :dnssd_epmd_stub.local_port_please()
     :dnssd.register(Atom.to_string(:erlang.node), "_epmd._tcp", local_ip)
   end
