019a827fe2cdc875715d466c69c2a504a7685150
Arjan Molenaar <gaphor@gmail.com>
2017-11-10 21:58:02 +0100

    Move compile task to bottom of file

--- mix.exs
+++ mix.exs
@@ -1,40 +1,9 @@
-defmodule Mix.Tasks.Compile.MdnsResponder do
-  def run(_args), do: make("all")
-
-  def clean(_args), do: make("clean")
-
-  defp make(target) do
-    Mix.shell.info("Compiling mdnsd binary...")
-    if match? {:win32, _}, :os.type do
-      # We do not support Windows fow now.
-      Mix.raise("Windows is not supported")
-    else
-      env = [
-        {"BUILD_DIR", Mix.Project.build_path()},
-        {"INSTALL_DIR", Mix.Project.build_path() <> "/lib/nerves_dnssd"}
-      ]
-
-      case System.cmd("make", [target], into: [], env: env) do
-        {_stdout, 0} -> :ok
-        {stdout, exit_code} ->
-          ["--------- Makefile output ----------\n"] ++
-            stdout ++
-            ["------ End of Makefile output ------\n"]
-          |> Enum.map(fn (line) -> IO.write line end)
-
-          Mix.raise("Build of mDNS daemon and library failed (#{inspect exit_code})")
-          exit({:shutdown, exit_code})
-      end
-    end
-  end
-end
-
 defmodule Nerves.Dnssd.Mixfile do
   use Mix.Project
 
   def project do
     [app: :nerves_dnssd,
-     version: "0.2.1",
+     version: "0.3.0",
      description: "Bonjour/Zeroconf DNS Service Discovery for the Nerves platform",
      elixir: "~> 1.5",
      build_embedded: true,
@@ -76,5 +45,36 @@ defmodule Nerves.Dnssd.Mixfile do
      maintainers: ["Arjan Molenaar"],
      files: ["lib", "src", "c_src", "LICENSE", "mix.exs", "Makefile", "README.md", "overview.md", "Changelog.md"],
      links: %{"GitHub" => "https://github.com/amolenaar/nerves_dnssd"}]
+  end
+end
+
+defmodule Mix.Tasks.Compile.MdnsResponder do
+  def run(_args), do: make("all")
+
+  def clean(_args), do: make("clean")
+
+  defp make(target) do
+    Mix.shell.info("Compiling mdnsd binary...")
+    if match? {:win32, _}, :os.type do
+      # We do not support Windows fow now.
+      Mix.raise("Windows is not supported")
+    else
+      env = [
+        {"BUILD_DIR", Mix.Project.build_path()},
+        {"INSTALL_DIR", Mix.Project.build_path() <> "/lib/nerves_dnssd"}
+      ]
+
+      case System.cmd("make", [target], into: [], env: env) do
+        {_stdout, 0} -> :ok
+        {stdout, exit_code} ->
+          ["--------- Makefile output ----------\n"] ++
+            stdout ++
+            ["------ End of Makefile output ------\n"]
+          |> Enum.map(fn (line) -> IO.write line end)
+
+          Mix.raise("Build of mDNS daemon and library failed (#{inspect exit_code})")
+          exit({:shutdown, exit_code})
+      end
+    end
   end
 end
